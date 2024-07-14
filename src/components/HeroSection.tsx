5b1a1b49311cf35ff1daea3f778fab150e855076
godson malu <godsonm2000@gmail.com>
2024-07-14 05:38:38 -0500

    Hero Section file change

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,8 +20,8 @@ const HeroSection = () => {
         </h1>
         <p className=" mt-4 font-normal text-base md:text-lg  from-neutral-50 to-neutral-500 max-w-lg mx-auto ">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you're beginner or loking to refine your true
-          potential.
+          journey today. Whether you&apos;re beginner or loking to refine your
+          true potential.
         </p>
         <div className="mt-4">
           <Link href={"/courses"}>
