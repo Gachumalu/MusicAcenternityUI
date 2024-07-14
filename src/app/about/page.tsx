7dc08070d238159be60f8d0a2fcc035cca9f6d0b
godson malu <godsonm2000@gmail.com>
2024-07-14 05:31:52 -0500

    About file change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,19 +8,19 @@ const About = () => {
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-2xl">
         <h1 className="text-4xl font-bold mb-4 text-center">About Us</h1>
         <p className="text-gray-700 mb-6">
-          Welcome to [Your Music Website]! We are dedicated to providing you
-          with the latest and greatest in music. Our team of passionate music
-          lovers works tirelessly to bring you the best in music news, reviews,
-          and more.
+          Welcome to Your Music Website We are dedicated to providing you with
+          the latest and greatest in music. Our team of passionate music lovers
+          works tirelessly to bring you the best in music news, reviews, and
+          more.
         </p>
         <p className="text-gray-700 mb-6">
           Our mission is to create a community of music enthusiasts who can
-          share and discover new music together. Whether you're into rock, pop,
-          jazz, or classical, we have something for everyone.
+          share and discover new music together. Whether you&apos;re into rock,
+          pop, jazz, or classical, we have something for everyone.
         </p>
         <p className="text-gray-700 mb-6">
           Thank you for visiting our site. We hope you enjoy your stay and find
-          something new and exciting to listen to!
+          something new and exciting to listen to.
         </p>
         <div className="text-center">
           <Link href="/" className="text-blue-500 hover:text-blue-700">
